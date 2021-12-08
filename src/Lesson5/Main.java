463cce066928ad03e608f233f67be0cc8c1b4f81
margarita <margarita-kolobkova@yandex.ru>
2021-12-08 23:18:20 +0500

    Домашнее задание 5

---
+++ src/Lesson5/Main.java
@@ -0,0 +1,15 @@
+package Lesson5;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        AppData data = new AppData();
+        data.readFromFile("instance.csv");
+        System.out.println(Arrays.toString(data.getHeader()));
+        System.out.println(Arrays.deepToString(data.getData()));
+
+        data.writeToCSV("new_instance.csv");
+    }
+
+}
